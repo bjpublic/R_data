fd9eba4193145e525a793aed308eb8afc61ac78c
jjy8517 <junyoung8517@naver.com>
2021-08-29 11:30:31 +0900

    Update 05. 지난 1년간 카페에는 어떤 일이 있었을까.R

--- script/05. 지난 1년간 카페에는 어떤 일이 있었을까.R
+++ script/05. 지난 1년간 카페에는 어떤 일이 있었을까.R
@@ -40,3 +40,42 @@ unique(sales$item)
 unique(sales$price)
 
 # 5.2 카페에서 가장 많이 판매한 메뉴 확인하기
+table(sales$item)
+
+# 내림차순 정렬하기
+sort(table(sales$item), decreasing = TRUE)
+
+sales_tr = data.frame(table(sales$item))
+head(sales_tr)
+
+sales_item = subset.data.frame(sales,
+                               select = c("item", "price"))
+
+head(sales_item)
+
+sales_item = unique(sales_item)
+sales_item
+
+# 잘못된 계산
+sales_tr$Freq*sales_item$price
+
+item_list = merge(sales_tr,
+                  sales_item,
+                  by.x = "Var1",
+                  by.y = "item")
+
+head(item_list)
+
+item_list$amount = item_list$Freq*item_list$price
+
+head(item_list)
+
+sum(item_list$amount)
+
+# 5.3 요일별로 판매한 메뉴 확인하기
+sales$weekday = weekdays(sales$order_date)
+
+head(sales)
+
+table(sales$weekday)
+
